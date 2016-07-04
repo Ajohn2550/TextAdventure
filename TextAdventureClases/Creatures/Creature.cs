153fc206f39b1bb8733985a1c4cd2c531d5963e7
Alexander Johnson <Bots@JohnsonWebWare.com>
2016-07-04 00:16:14 -0700

    Heal and TakeDamage

--- TextAdventureClases/Creatures/Creature.cs
+++ TextAdventureClases/Creatures/Creature.cs
@@ -10,5 +10,35 @@ namespace TextAdventureClases.Creatures
     {
         public string Name { get; set; }
         public int Health { get; set; }
+
+        public Creature()
+        {
+            Name = "Unknown Creature";
+            Health = 100;
+        }
+
+        public Creature(string name)
+        {
+            Name = name;
+            Health = 100;
+        }
+
+        public Creature(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health = Health - damage;
+        }
+        
+        public int Heal(int min, int max)
+        {
+            int healed = new Random().Next(min, max);
+            Health = Health + healed;
+            return healed;
+        }
     }
 }
---
+++ UnitTest/Classes/Creatures/CreatureTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextAdventureClases.Creatures;
+
+namespace UnitTest.Classes.Creatures
+{
+    [TestClass]
+    public class CreatureTests
+    {
+        [TestMethod]
+        public void CanTakeDamage()
+        {
+            var creature = new Creature();
+            int damage = 10;
+            int startHealth = creature.Health;
+            creature.TakeDamage(damage);
+
+            Assert.IsTrue(creature.Health == startHealth - damage, "Dmage Taken");
+        }
+
+        [TestMethod]
+        public void CanHeal()
+        {
+            var creature = new Creature();
+            int startHealth = creature.Health;
+            int healed = creature.Heal(1, 10);
+
+            Assert.IsTrue(healed > 0, "Healed more than 0 hp.");
+            Assert.IsTrue(creature.Health > startHealth, "Health Increased");
+        }
+    }
+}
