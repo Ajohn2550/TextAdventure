029b0481e23bfcac7cb9f12debd4b8365e8e8361
Alexander Johnson <Bots@JohnsonWebWare.com>
2016-07-05 00:51:55 -0700

    Adding stats

--- TextAdventureClases/Creatures/Creature.cs
+++ TextAdventureClases/Creatures/Creature.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextAdventureClases.Creatures
 {
@@ -10,25 +6,39 @@ namespace TextAdventureClases.Creatures
     {
         public string Name { get; set; }
         public int Health { get; set; }
+        public int Level { get; set; }
+        public int Strength { get; set; }
+        public int Defense { get; set; }
 
+        #region Creature Constructors
         public Creature()
         {
             Name = "Unknown Creature";
             Health = 100;
+            Level = 1;
+            Strength = 1;
+            Defense = 1;
         }
 
         public Creature(string name)
         {
             Name = name;
             Health = 100;
+            Level = 1;
+            Strength = 1;
+            Defense = 1;
         }
 
-        public Creature(string name, int health)
+        public Creature(string name, int health, int level, int strength, int defense)
         {
             Name = name;
             Health = health;
+            Level = level;
+            Strength = strength;
+            Defense = defense;
         }
-
+        #endregion
+        #region Health Methods
         public void TakeDamage(int damage)
         {
             Health = Health - damage;
@@ -40,5 +50,15 @@ namespace TextAdventureClases.Creatures
             Health = Health + healed;
             return healed;
         }
+        #endregion
+        #region Level Methods
+        public int LevelUp()
+        {
+            return ++Level;
+        }
+        #endregion
+        #region Strength Methods
+
+        #endregion
     }
 }
